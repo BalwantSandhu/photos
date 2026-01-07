53409aa4d6a725e2288dca3ed874a321215492d1
BalwantSandhu <bsingh3@unb.ca>
2026-01-07 00:56:10 -0400

    genarted photo component and service to fetch photo

---
+++ src/app/photo-show/photo-show.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-show',
+  imports: [],
+  templateUrl: './photo-show.component.html',
+  styleUrl: './photo-show.component.css'
+})
+export class PhotoShowComponent {
+
+}
